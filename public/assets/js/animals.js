3c33351951d08330c81c4f688581838d2105e62e
Kashay Arbelo <kashay@agyeman.me>
2021-02-23 19:54:48 -0700

    added additional code needed to animals.js to GET and POST form submissions and original JSON data

--- public/assets/js/animals.js
+++ public/assets/js/animals.js
@@ -22,6 +22,7 @@ const printResults = resultArr => {
   $displayArea.innerHTML = animalHTML.join('');
 };
 
+/*/ This function is actually capable of making two types of requests; it will depend on how the `queryUrl` ends up looking. If nothing is passed into formData, then the request will be simply GET `/api/animals`. This will be what runs on load. ---  we call the `handleGetAnimalsSubmit()` function on the add new animal form submission   ---   This function will gather all of the form input data and package it as an object to send to the `getAnimals()` function as the formData argument. From there, the object formData will be passed through the `Object.entries()` method to create query parameters. Because all that work is done for us, all we have to do is make the request using `queryUrl`! */
 const getAnimals = (formData = {}) => {
   let queryUrl = '/api/animals?';
 
@@ -30,6 +31,20 @@ const getAnimals = (formData = {}) => {
   });
 
   console.log(queryUrl);
+
+  /*   Remember, when using fetch() we have to check to see if the ok property in the response is true or false. This is the part that will check for any HTTP status code the signifies an error. If there's an error, we just alert the user that there's something wrong. If everything's okay, we still have to use the .json() method to parse our response into readable JSON format.  ---   When that's all done, we send our array of animal data to the printResults() function, where it generates cards for each animal and prints them to the page!   */
+
+  fetch(queryUrl)
+  .then(response => {
+    if (!response.ok) {
+      return alert('Error: ' + response.statusText);
+    }
+    return response.json();
+  })
+  .then(animalData => {
+    console.log(animalData);
+    printResults(animalData);
+  }); 
 
 };
 
